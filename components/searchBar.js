186c407c43a4dd9c93ed7017351066c4a1bc941e
Colin FitzGerald <colinfitzgerald328@gmail.com>
2023-01-31 08:21:18 -0800

    add athlete

--- components/searchBar.js
+++ components/searchBar.js
@@ -1,14 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import { Input, Button, Table } from "@nextui-org/react";
+import { toaster } from 'evergreen-ui'
 import styles from "./styles.module.css";
 
 const SearchBar = () => {
   const [query, setQuery] = useState('');
   const [name, setName] = useState(null);
+  const [athleteName, setAthleteName] = useState(null);
   const [results, setResults] = useState([]);
+  const [response, setResponse] = useState(null);
+
 
   const handleSearch = async () => {
-    console.log(query)
     const response = await fetch(`https://world-athletics-376305.uc.r.appspot.com/getAthleteMarks?athlete_name=${encodeURI(query)}`);
     const data = await response.json();
     console.log
@@ -26,6 +29,24 @@ const SearchBar = () => {
       label: "Time",
     }
   ];
+
+  const handleAthleteAdd = async () => {
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ "athlete_name": athleteName })
+  };
+  fetch('https://world-athletics-376305.uc.r.appspot.com/addAthlete', requestOptions)
+      .then(response => response.json())
+      .then(data => setResponse({ data: data }));
+      if (response["data"]["operation"] == "success") {
+        toaster.success('Athlete added!')
+      } else {
+        toaster.danger('Failed to add athlete. Please email colinfitzgerald@berkeley.edu for help.')
+      }
+  };
+
+  
 
   return (
     <div className={styles.main}>
@@ -60,6 +81,19 @@ const SearchBar = () => {
                     )}
                 </Table.Body></Table>
       </div>
+      <div className={styles.searchBar}>
+        Athlete you searched showing no results? They may not be in our database yet. 
+        Add them here! 
+      </div>
+      <Input
+        type="text"
+        placeholder="Athlete name..."
+        value={athleteName}
+        onChange={(event) => setAthleteName(event.target.value)}
+      />
+      <div className={styles.button}>
+      <Button onClick={handleAthleteAdd}>Add</Button>
+       </div>
     </div>
   );
 };
